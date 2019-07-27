87e16099b6aefa8b375ef8d67cdee7e222ffcf37
rockstarcreativestudio <support@rockstarcreativestudio.com>
2019-07-27 09:29:11 -0400

    Updates

---
+++ precache-manifest.eef4e25423fef9fb2a7cffe94c52f114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa14b6fe657b5d2ba06e80a827c206",
+    "url": "/presidential-campaign-finance-2020/index.html"
+  },
+  {
+    "revision": "ff59c46d139ed136fe76",
+    "url": "/presidential-campaign-finance-2020/static/css/main.d1e4ef6e.chunk.css"
+  },
+  {
+    "revision": "4d55e68433f5c68f5b5d",
+    "url": "/presidential-campaign-finance-2020/static/js/2.576c3599.chunk.js"
+  },
+  {
+    "revision": "ff59c46d139ed136fe76",
+    "url": "/presidential-campaign-finance-2020/static/js/main.524bf506.chunk.js"
+  },
+  {
+    "revision": "94eb7ce273629d4a94a5",
+    "url": "/presidential-campaign-finance-2020/static/js/runtime~main.1941d807.js"
+  }
+]);
