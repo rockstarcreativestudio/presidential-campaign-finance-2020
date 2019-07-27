d28a794cce88883d383d355c93aabb5fd2890bf3
rockstarcreativestudio <support@rockstarcreativestudio.com>
2019-07-27 11:07:02 -0400

    Updates

--- precache-manifest.575706af77b4298fd6d75eb142ae619d.js
+++ precache-manifest.575706af77b4298fd6d75eb142ae619d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f0f6c377b3e86358e1fb5e49eb09113",
+    "revision": "a7c53397b0b610ec85167ce2df0ca2d4",
     "url": "/presidential-campaign-finance-2020/index.html"
   },
   {
-    "revision": "3b0806de63cd5cec92b6",
+    "revision": "21f8a65473c20d114889",
     "url": "/presidential-campaign-finance-2020/static/css/main.321f663a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/presidential-campaign-finance-2020/static/js/2.c385d5bc.chunk.js"
   },
   {
-    "revision": "3b0806de63cd5cec92b6",
-    "url": "/presidential-campaign-finance-2020/static/js/main.5fe2f73a.chunk.js"
+    "revision": "21f8a65473c20d114889",
+    "url": "/presidential-campaign-finance-2020/static/js/main.6b8af986.chunk.js"
   },
   {
     "revision": "94eb7ce273629d4a94a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/presidential-campaign-finance-2020/precache-manifest.49e0009ec649e6d055333b0b1acd0608.js"
+  "/presidential-campaign-finance-2020/precache-manifest.575706af77b4298fd6d75eb142ae619d.js"
 );
 
 self.addEventListener('message', (event) => {
